eae21bcf4f858ce95d2caab1decb53b27c70f040
bugzyz <bugzyz@hotmail.com>
2018-11-22 20:46:41 +0800

    add traverse and collect information code in debug.hpp

--- debug.hpp
+++ debug.hpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
 #include <unistd.h>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 namespace myTrie {
 namespace debuging {
@@ -97,6 +97,122 @@ void process_mem_usage(double& vm_usage, double& resident_set) {
 }  // namespace debuging
 }  // namespace myTrie
 
+#include "myTrie.hpp"
+
+namespace myTrie {
+namespace debuging {
+using std::cout;
+using std::endl;
+uint64_t h_n;
+uint64_t t_n;
+uint64_t hash_node_mem;
+uint64_t trie_node_mem;
+
+size_t hashnode_load = 0;
+size_t hashnode_max_load = Max_slot_num / 2;
+size_t hashnode_min_load = Max_slot_num / 2;
+
+template <typename CharT, typename T>
+void print_tree_construct(typename myTrie::htrie_map<CharT, T>::anode* root) {
+    using my = typename myTrie::htrie_map<CharT, T>;
+    if (root == nullptr) {
+        return;
+    }
+    // print bucket
+    if (root->isHashNode()) {
+        hash_node_mem += sizeof(root);
+
+        // basic bucket cost
+        uint32_t bucket_mem =
+            sizeof(typename my::hash_node::slot) * Max_slot_num;
+        hash_node_mem += bucket_mem;
+
+        // page mem cost
+        size_t pages_cost =
+            Max_bytes_per_kv * ((typename my::hash_node*)root)->pages.size();
+        hash_node_mem += pages_cost;
+
+        h_n++;
+
+        size_t current_elem_num = ((typename my::hash_node*)root)->elem_num;
+        hashnode_load += current_elem_num;
+        if (current_elem_num > hashnode_max_load) {
+            hashnode_max_load = current_elem_num;
+        }
+
+        if (current_elem_num < hashnode_min_load) {
+            hashnode_min_load = current_elem_num;
+        }
+
+    } else if (root->isTrieNode()) {
+        trie_node_mem += sizeof(root);
+
+        std::map<CharT, typename myTrie::htrie_map<CharT, T>::trie_node*>
+            childs = ((typename myTrie::htrie_map<CharT, T>::trie_node*)root)
+                         ->childs;
+        if (childs.size() == 0) {
+            print_tree_construct<CharT, T>(
+                ((typename myTrie::htrie_map<CharT, T>::trie_node*)root)
+                    ->getOnlyHashNode());
+        } else {
+            for (auto it = childs.begin(); it != childs.end(); it++) {
+                t_n++;
+                print_tree_construct<CharT, T>(it->second);
+            }
+        }
+    } else {
+        std::cout << "node is not trie nor hash node\n";
+        exit(0);
+    }
+    return;
+}
+
+void clear_num() {
+    t_n = 0;
+    h_n = 0;
+    hash_node_mem = 0;
+    trie_node_mem = 0;
+    hashnode_load = 0;
+    hashnode_max_load = Max_slot_num / 2;
+    hashnode_min_load = Max_slot_num / 2;
+    return;
+}
+
+template <typename CharT, typename T>
+double print_res() {
+    std::cout << "trie_node: " << t_n << " hash_node: " << h_n << std::endl;
+    std::cout << "trie_node_mem: " << trie_node_mem
+              << " hash_node_mem: " << hash_node_mem << std::endl;
+    using my = typename myTrie::htrie_map<CharT, T>;
+
+    std::cout << "total: ," << (hash_node_mem + trie_node_mem) / 1024 / 1024
+              << ", mb" << std::endl;
+    double ret_v;
+    ret_v = (hash_node_mem + trie_node_mem) / 1024 / 1024;
+
+    // todo
+    // std::cout << "trienode size:" << sizeof(typename my::trie_node)
+    //           << std::endl;
+    // std::cout << "hashnode size:" << sizeof(typename my::hash_node)
+    //           << std::endl;
+    // std::cout << "arraybucket size:" << sizeof(typename my::array_bucket)
+    //           << std::endl;
+    // std::cout << "htrie_map size:"
+    //           << sizeof(typename myTrie::htrie_map<CharT, T>) << std::endl;
+    // std::cout
+    //     << "htrie_map::v2k size:"
+    //     << sizeof(
+    //            std::map<T, typename myTrie::htrie_map<CharT,
+    //            T>::SearchPoint>)
+    //     << std::endl;
+    // std::cout << "arraybucket::kvs size:"
+    //           << sizeof(std::vector<typename my::array_bucket>) << std::endl;
+
+    return ret_v;
+}
+}  // namespace debuging
+}  // namespace myTrie
+
 uint64_t get_usec() {
     struct timespec tp;
     /* POSIX.1-2008: Applications should use the clock_gettime() function
@@ -106,7 +222,6 @@ uint64_t get_usec() {
     clock_gettime(CLOCK_MONOTONIC, &tp);
     return ((tp.tv_sec * 1000 * 1000) + (tp.tv_nsec / 1000));
 }
-
 
 #include <sys/sysinfo.h>
 uint64_t getLftMem() {
