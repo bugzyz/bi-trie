efadfa08e2482842174e61de5eb9099e28a10bcc
bugzyz <bugzyz@hotmail.com>
2019-01-11 10:30:13 +0800

    split get_time() to individual hpp

---
+++ util/my_timer.hpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <time.h>
+
+uint64_t get_time() {
+    struct timespec tp;
+    /* POSIX.1-2008: Applications should use the clock_gettime() function
+       instead of the obsolescent gettimeofday() function. */
+    /* NOTE: The clock_gettime() function is only available on Linux.
+       The mach_absolute_time() function is an alternative on OSX. */
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    return ((tp.tv_sec * 1000 * 1000) + (tp.tv_nsec / 1000));
+}
